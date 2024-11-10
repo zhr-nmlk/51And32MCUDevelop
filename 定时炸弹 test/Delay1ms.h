ffae3770156330647964d344ca758d2df5ff8d7e
zhr_nmlk <zl341190392@gmail.com>
2024-11-10 10:38:16 +0800

    完善readme，整理文件

--- 定时炸弹 test/Delay1ms.h
+++
@@ -1,7 +0,0 @@
-#ifndef __Delay1ms_H__
-#define __Delay1ms_H__
-
-void Delay1ms(unsigned int xms);
-void Delay100us();
-
-#endif
--- 定时炸弹 test/UART.h
+++
@@ -1,7 +0,0 @@
-#ifndef __UART_H__
-#define __UART_H__
-
-void UartInit();
-void UART_SendByte(unsigned char Byte);
-
-#endif
